dd014450ee9c15e8fb7524b24b674cc553ec9e55
saeki <shingen7303@gmail.com>
2019-05-21 20:31:16 +0900

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # メッセージが来た時、友達追加・解除がされた時に、linebotコントローラのcallbackアクションが呼ばれる
+  post '/callback' => 'linebot#callback'
 end
